348c24aa7d347403a709df454eac9c8fc30e498f
heunseoRyu <heunseo9@gmail.com>
2024-03-10 19:02:02 +0900

    Feat::First Commit

---
+++ src/main/java/ducami/org/ducademi/DucademiApplication.java
@@ -0,0 +1,13 @@
+package ducami.org.ducademi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucademiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucademiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ducami/org/ducademi/DucademiApplicationTests.java
@@ -0,0 +1,13 @@
+package ducami.org.ducademi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucademiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
