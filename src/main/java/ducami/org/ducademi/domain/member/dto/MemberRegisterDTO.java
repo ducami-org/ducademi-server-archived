26ef06e006d66014821e1c1a097bc1b0776e0167
이슬아 <rheeseula@gmail.com>
2024-04-02 23:17:15 +0900

    Feat :: Member 도메인 RegisterDTO 추가

---
+++ src/main/java/ducami/org/ducademi/domain/member/dto/MemberRegisterDTO.java
@@ -0,0 +1,14 @@
+package ducami.org.ducademi.domain.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberRegisterDTO {
+
+    private String id;
+    private String password;
+    private String name;
+    private String birth;
+    private String phoneNum;
+
+}
