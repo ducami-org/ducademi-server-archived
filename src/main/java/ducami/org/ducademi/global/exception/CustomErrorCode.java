c1a499182cb56f3b8b10d7fb76ce9c779bf8927a
이슬아 <rheeseula@gmail.com>
2024-04-03 23:30:01 +0900

    Feat :: Global CustomErrorCode JWT_WAS_EXPIRED 추가
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/java/ducami/org/ducademi/global/exception/CustomErrorCode.java
+++ src/main/java/ducami/org/ducademi/global/exception/CustomErrorCode.java
@@ -11,7 +11,10 @@ public enum CustomErrorCode {
     // 멤버 관련 오류
     MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "M-1", "멤버가 이미 존재한다"),
     MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M-2", "멤버가 존재하지 않는다"),
-    MEMBER_NOT_CORRECT(HttpStatus.NOT_FOUND, "M-3", "멤버 정보가 일치하지 않음");
+    MEMBER_NOT_CORRECT(HttpStatus.NOT_FOUND, "M-3", "멤버 정보가 일치하지 않다"),
+
+    // JWT 관련 오류
+    JWT_WAS_EXPIRED(HttpStatus.FORBIDDEN, "J-1", "토큰이 만료되었다");
 
     private final HttpStatus code;
     private final String status;
