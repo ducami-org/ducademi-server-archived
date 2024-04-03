3ebf26c4b8cc0c46eedad134e5d116608587a3e6
이슬아 <rheeseula@gmail.com>
2024-04-03 20:33:35 +0900

    Feat :: Member 도메인 Service 로그인 로직 추가

--- src/main/java/ducami/org/ducademi/domain/member/service/MemberService.java
+++ src/main/java/ducami/org/ducademi/domain/member/service/MemberService.java
@@ -1,9 +1,12 @@
 package ducami.org.ducademi.domain.member.service;
 
 import ducami.org.ducademi.domain.member.authority.MemberAccountType;
+import ducami.org.ducademi.domain.member.dto.MemberLoginDTO;
 import ducami.org.ducademi.domain.member.dto.MemberRegisterDTO;
 import ducami.org.ducademi.domain.member.entity.MemberEntity;
 import ducami.org.ducademi.domain.member.repository.MemberRepository;
+import ducami.org.ducademi.global.auth.JwtInfo;
+import ducami.org.ducademi.global.auth.JwtUtils;
 import ducami.org.ducademi.global.exception.CustomErrorCode;
 import ducami.org.ducademi.global.exception.CustomException;
 import ducami.org.ducademi.global.response.BaseResponse;
@@ -12,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtUtils jwtUtils;
 
     public BaseResponse<?> register(MemberRegisterDTO dto) {
         if (memberRepository.existsById(dto.getId())) {
@@ -43,6 +49,24 @@ public class MemberService {
                 "OK",
                 "회원가입 성공",
                 null
+        );
+    }
+
+    public BaseResponse<?> login(MemberLoginDTO dto) {
+        MemberEntity member = memberRepository.findById(dto.getId())
+                .orElseThrow(() -> new CustomException(CustomErrorCode.MEMBER_NOT_EXIST));
+
+        if (!bCryptPasswordEncoder.matches(dto.getPassword(), member.getPassword())) {
+            throw new CustomException(CustomErrorCode.MEMBER_NOT_CORRECT);
+        }
+
+        return BaseResponse.of(
+                true,
+                "OK",
+                "로그인 성공",
+                List.of(
+                        jwtUtils.generateToken(member)
+                )
         );
     }
 
