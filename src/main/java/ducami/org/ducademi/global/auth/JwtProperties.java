09e1c2ee7575d5b9294f79c0f30dcc9c125f1a57
이슬아 <rheeseula@gmail.com>
2024-04-03 20:34:44 +0900

    Feat :: Global Auth JwtProperties 추가

---
+++ src/main/java/ducami/org/ducademi/global/auth/JwtProperties.java
@@ -0,0 +1,17 @@
+package ducami.org.ducademi.global.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class JwtProperties {
+
+    @Value("${jwt.secret}") private String secretKey;
+    @Value("${jwt.expire.access}") private Long accessExpired;
+    @Value("${jwt.expire.refresh}") private Long refreshExpired;
+
+}
