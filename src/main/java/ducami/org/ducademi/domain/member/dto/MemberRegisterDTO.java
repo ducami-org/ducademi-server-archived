7243235d676555c46854a9f6da7242170474ccf7
이슬아 <rheeseula@gmail.com>
2024-04-03 19:36:31 +0900

    Feat :: Member 도메인 회원가입 로직 Validation 추가

--- src/main/java/ducami/org/ducademi/domain/member/dto/MemberRegisterDTO.java
+++ src/main/java/ducami/org/ducademi/domain/member/dto/MemberRegisterDTO.java
@@ -1,14 +1,24 @@
 package ducami.org.ducademi.domain.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class MemberRegisterDTO {
 
+    @NotBlank
     private String id;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String birth;
+
+    @NotBlank
     private String phoneNum;
 
 }
