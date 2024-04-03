681ae5a84ed9b2e3fe1aa5ebd20d2c868518b400
이슬아 <rheeseula@gmail.com>
2024-04-03 23:29:20 +0900

    Feat :: Member 도메인 AccounType enum 클래스 convert 메서드 추가
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/java/ducami/org/ducademi/domain/member/authority/MemberAccountType.java
+++ src/main/java/ducami/org/ducademi/domain/member/authority/MemberAccountType.java
@@ -1,9 +1,25 @@
 package ducami.org.ducademi.domain.member.authority;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum MemberAccountType {
 
     ROLE_USER,
     ROLE_DUCAMI,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    public static GrantedAuthority convert(MemberAccountType type) {
+        switch (type) {
+            case ROLE_USER :
+                return new SimpleGrantedAuthority("ROLE_USER");
+            case ROLE_DUCAMI :
+                return new SimpleGrantedAuthority("ROLE_DUCAMI");
+            case ROLE_ADMIN :
+                return new SimpleGrantedAuthority("ROLE_ADMIN");
+            default :
+                throw new IllegalArgumentException("잘못된 권한 이름");
+        }
+    }
 
 }
