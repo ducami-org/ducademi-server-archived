4cfe00eff2f9e8b877b1f49ec3c79ae1d823fa0f
이슬아 <rheeseula@gmail.com>
2024-04-03 23:26:06 +0900

    Feat :: Global SpringConfig에 ObjectMapper 추가

--- src/main/java/ducami/org/ducademi/global/config/SpringConfig.java
+++ src/main/java/ducami/org/ducademi/global/config/SpringConfig.java
@@ -1,5 +1,6 @@
 package ducami.org.ducademi.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,11 @@ public class SpringConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 
 }
