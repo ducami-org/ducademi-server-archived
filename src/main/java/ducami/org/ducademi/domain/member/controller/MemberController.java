cb84317b7cbecff90d71608b2c4bfb5919399f98
이슬아 <rheeseula@gmail.com>
2024-04-06 20:54:04 +0900

    Feat :: Member 도메인 Controller OAuth2 Implement

--- src/main/java/ducami/org/ducademi/domain/member/controller/MemberController.java
+++ src/main/java/ducami/org/ducademi/domain/member/controller/MemberController.java
@@ -6,10 +6,7 @@ import ducami.org.ducademi.domain.member.service.MemberService;
 import ducami.org.ducademi.global.response.BaseResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("member")
@@ -26,6 +23,11 @@ public class MemberController {
     @PostMapping("login")
     public BaseResponse<?> postLogin(@Valid @RequestBody MemberLoginDTO dto) {
         return memberService.postLogin(dto);
+    }
+
+    @GetMapping("oauth2")
+    public void getOAuth2(String code, String registrationId) {
+
     }
 
 }
