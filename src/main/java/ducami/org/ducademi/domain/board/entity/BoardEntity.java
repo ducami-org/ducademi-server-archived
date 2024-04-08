29e4f23aa6c5e2407c59c95d5edfa461c8c2c967
김호준 <hojoon4868@gmail.com>
2024-04-08 18:16:12 +0900

    Feat :: Board 도메인 BoardEntity 생성

---
+++ src/main/java/ducami/org/ducademi/domain/board/entity/BoardEntity.java
@@ -0,0 +1,40 @@
+package ducami.org.ducademi.domain.board.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "board")
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardIdx;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(nullable = false)
+    private LocalDateTime modifiedDate;
+
+
+}
