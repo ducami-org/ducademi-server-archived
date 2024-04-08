60c495cceafeafc9b400f4df44272e6d1f2a7b15
heunseoRyu <heunseo9@gmail.com>
2024-04-08 21:55:35 +0900

    Feat :: DucademiApplication 수정

--- src/main/java/ducami/org/ducademi/DucademiApplication.java
+++ src/main/java/ducami/org/ducademi/DucademiApplication.java
@@ -2,8 +2,12 @@ package ducami.org.ducademi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class DucademiApplication {
 
 	public static void main(String[] args) {
