2aa2a3458ca0ad46abfebc911bd72f2f13512afc
김호준 <hojoon4868@gmail.com>
2024-04-08 20:57:11 +0900

    Feat :: Board 도메인 FileRepository 생성

---
+++ src/main/java/ducami/org/ducademi/domain/board/repository/FileRepository.java
@@ -0,0 +1,8 @@
+package ducami.org.ducademi.domain.board.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FileRepository extends CrudRepository<FileRepository, Long> {
+
+}
