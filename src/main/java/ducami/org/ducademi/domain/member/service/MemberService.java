c80905a248c316e9e45f03269b54789c7c8afd74
이슬아 <rheeseula@gmail.com>
2024-04-10 20:53:12 +0900

    Feat :: Member 도메인 Service 구글 OAuth2 액세스 토큰 발급
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/java/ducami/org/ducademi/domain/member/service/MemberService.java
+++ src/main/java/ducami/org/ducademi/domain/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package ducami.org.ducademi.domain.member.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import ducami.org.ducademi.domain.member.authority.MemberAccountType;
 import ducami.org.ducademi.domain.member.dto.MemberLoginDTO;
 import ducami.org.ducademi.domain.member.dto.MemberRegisterDTO;
@@ -8,13 +9,18 @@ import ducami.org.ducademi.domain.member.repository.MemberRepository;
 import ducami.org.ducademi.global.auth.JwtUtils;
 import ducami.org.ducademi.global.exception.CustomErrorCode;
 import ducami.org.ducademi.global.exception.CustomException;
+import ducami.org.ducademi.global.property.OAuth2Properties;
 import ducami.org.ducademi.global.response.BaseResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -24,6 +30,8 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final JwtUtils jwtUtils;
+    private final RestTemplate restTemplate;
+    private final OAuth2Properties oAuth2Properties;
 
     public BaseResponse<?> postRegister(MemberRegisterDTO dto) {
         if (memberRepository.existsById(dto.getId())) {
@@ -68,6 +76,34 @@ public class MemberService {
                         jwtUtils.generateToken(member)
                 )
         );
+    }
+
+    public void getOAuth2(String code, String registrationId) {
+        String token = this.getAccessToken(code);
+    }
+
+    public String getAccessToken(String code) {
+        Map<String, String> params = new HashMap<>();
+
+        params.put("code", code);
+        params.put("client_id", oAuth2Properties.getGoogleId());
+        params.put("client_secret", oAuth2Properties.getGoogleSecret());
+        params.put("redirect-uri", oAuth2Properties.getGoogleRedirectURI());
+        params.put("grant_type", "authorization_code");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<?> httpEntity = new HttpEntity<>(params, httpHeaders);
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate.exchange(
+                oAuth2Properties.getGoogleTokenURI(),
+                HttpMethod.POST,
+                httpEntity,
+                JsonNode.class
+        );
+
+        return responseEntity.getBody().get("access_token").asText();
     }
 
 }
