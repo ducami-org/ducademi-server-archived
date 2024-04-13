b007c59f621fd02fc95129679ae025e445c0350f
whitenoise13 <amnesiamar13@gmail.com>
2024-04-14 06:06:16 +0900

    Fix :: 교재 BookRepository 수정

--- src/main/java/ducami/org/ducademi/domain/book/repository/BookRepository.java
+++ src/main/java/ducami/org/ducademi/domain/book/repository/BookRepository.java
@@ -1,13 +1,23 @@
 package ducami.org.ducademi.domain.book.repository;
 
+
+import ducami.org.ducademi.domain.book.category.BookCategory;
 import ducami.org.ducademi.domain.book.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
-    List<BookEntity> findByCreationDateOderOrderByCreationDateDesc(LocalDate creationDate);
-    List<BookEntity> findByCategory(String category);
-    List<BookEntity> findByTitleContaining(String title);
+
+    // 모든 교재 조회
+    List<BookEntity> findAllBooks();
+
+    // 카테고리별 교재 조회
+    List<BookEntity> findByCategory(BookCategory category);
+
+    // 교재 제목으로 조회
+    List<BookEntity> findByTitle(String title);
+
 }
