5f93f56e5d4b3a37994897521952a4b7f445676e
whitenoise13 <amnesiamar13@gmail.com>
2024-04-14 06:06:04 +0900

    Fix :: 교재 BookDTO 수정

--- src/main/java/ducami/org/ducademi/domain/book/dto/BookDTO.java
+++ src/main/java/ducami/org/ducademi/domain/book/dto/BookDTO.java
@@ -1,6 +1,7 @@
 package ducami.org.ducademi.domain.book.dto;
 
 import ducami.org.ducademi.domain.book.category.BookCategory;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +10,24 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class BookDTO {
+    @NotBlank
     private Long bookIdx;
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private LocalDate createdDate;
+
+    @NotBlank
     private String bookCover;
+
+    @NotBlank
     private String introduction;
+
+    @NotBlank
     private BookCategory category;
+
+    @NotBlank
     private LocalDate modifiedDate;
 }
