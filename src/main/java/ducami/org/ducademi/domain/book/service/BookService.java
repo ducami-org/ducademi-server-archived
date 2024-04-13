f4a315dc43b1e5b4537fea8f44e0dc10e168aa06
whitenoise13 <amnesiamar13@gmail.com>
2024-04-14 06:06:26 +0900

    Fix :: 교재 BookService 수정

--- src/main/java/ducami/org/ducademi/domain/book/service/BookService.java
+++ src/main/java/ducami/org/ducademi/domain/book/service/BookService.java
@@ -1,29 +1,55 @@
 package ducami.org.ducademi.domain.book.service;
 
+import ducami.org.ducademi.domain.book.category.BookCategory;
+import ducami.org.ducademi.domain.book.dto.BookDTO;
 import ducami.org.ducademi.domain.book.entity.BookEntity;
 import ducami.org.ducademi.domain.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
-    private final BookRepository BookRepository;
 
-    public BookService(BookRepository BookRepository) {
-        this.BookRepository = BookRepository;
-    }
+    private final BookRepository BOOK_REPOSITORY;
 
+
+    // 모든 교재 조회
     public List<BookEntity> getAllBooks() {
-        return BookRepository.findAll();
+        return BOOK_REPOSITORY.findAllBooks();
     }
 
-    public List<BookEntity> getBooksByCategory(String category) {
-        return BookRepository.findByCategory(category);
+    // 특정 카테고리의 교재 조회
+    public List<BookDTO> getBooksByCategory(BookCategory category) {
+        List<BookEntity> bookEntities = BOOK_REPOSITORY.findByCategory(category);
+        return bookEntities.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
-    public List<BookEntity> searchBooksByTitle(String title) {
-        return BookRepository.findByTitleContaining(title);
+    // 교재 제목으로 검색
+    public List<BookDTO> searchBooksByTitle(String title) {
+        List<BookEntity> bookEntities = BOOK_REPOSITORY.findByTitle(title);
+        return bookEntities.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+
+    public BookDTO convertToDTO(BookEntity bookEntity) {
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setBookIdx(bookEntity.getBookIdx());
+        bookDTO.setTitle(bookEntity.getTitle());
+        bookDTO.setCreatedDate(bookEntity.getCreatedDate());
+        bookDTO.setBookCover(bookEntity.getBookCover());
+        bookDTO.setIntroduction(bookEntity.getIntroduction());
+        bookDTO.setCategory(bookEntity.getCategory());
+        bookDTO.setModifiedDate(bookEntity.getModifiedDate());
+        return bookDTO;
     }
 
 }
