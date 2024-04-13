986c83f66986483e9d6b9494661c6b744127570d
whitenoise13 <amnesiamar13@gmail.com>
2024-04-14 06:03:39 +0900

    Fix :: 교재 BookController 수정

--- src/main/java/ducami/org/ducademi/domain/book/controller/BookController.java
+++ src/main/java/ducami/org/ducademi/domain/book/controller/BookController.java
@@ -1,33 +1,49 @@
 package ducami.org.ducademi.domain.book.controller;
 
 
+import ducami.org.ducademi.domain.book.category.BookCategory;
+import ducami.org.ducademi.domain.book.dto.BookDTO;
 import ducami.org.ducademi.domain.book.entity.BookEntity;
 import ducami.org.ducademi.domain.book.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
-    private final BookService bookService;
 
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
+    private final BookService BOOK_SERVICE;
 
-    @GetMapping
-    public List<BookEntity> getAllBook() {
-        return bookService.getAllBooks();
-    }
-
-    @GetMapping("/search")
-    public List<BookEntity> getSearchBookByTitle(@RequestParam String title) {
-        return bookService.searchBooksByTitle(title);
+    @GetMapping("/all")
+    public List<BookDTO> getAllBooks() {
+        List<BookEntity> bookEntities = BOOK_SERVICE.getAllBooks();
+        return bookEntities.stream()
+                .map(BOOK_SERVICE::convertToDTO)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/category/{category}")
-    public List<BookEntity> getBooksByCategory(@PathVariable String category) {
-        return bookService.getBooksByCategory(category);
+    public List<BookDTO> getBooksByCategory(@PathVariable String category) {
+        BookCategory bookCategory = null;
+        for (BookCategory cat : BookCategory.values()) {
+            if (cat.name().equalsIgnoreCase(category)) {
+                bookCategory = cat;
+                break;
+            }
+        }
+        if (bookCategory == null) {
+            throw new IllegalArgumentException("존재하지 않는 카테고리: " + category);
+        }
+        return BOOK_SERVICE.getBooksByCategory(bookCategory);
     }
+
+    @GetMapping("/search")
+    public List<BookDTO> getSearchBooksByTitle(@RequestParam String title) {
+        return BOOK_SERVICE.searchBooksByTitle(title);
+    }
+
 }
