0240d0a64e2edb9f8f14b07cebe6bc69dd74544d
whitenoise13 <amnesiamar13@gmail.com>
2024-04-15 19:28:40 +0900

    Fix :: 교재 BookController 수정

--- src/main/java/ducami/org/ducademi/domain/book/controller/BookController.java
+++ src/main/java/ducami/org/ducademi/domain/book/controller/BookController.java
@@ -6,6 +6,8 @@ import ducami.org.ducademi.domain.book.dto.BookDTO;
 import ducami.org.ducademi.domain.book.entity.BookEntity;
 import ducami.org.ducademi.domain.book.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +20,7 @@ public class BookController {
 
     private final BookService BOOK_SERVICE;
 
+    // 모든 교재 조회
     @GetMapping("/all")
     public List<BookDTO> getAllBooks() {
         List<BookEntity> bookEntities = BOOK_SERVICE.getAllBooks();
@@ -26,6 +29,8 @@ public class BookController {
                 .collect(Collectors.toList());
     }
 
+
+    // 카테고리별 교재 조회
     @GetMapping("/category/{category}")
     public List<BookDTO> getBooksByCategory(@PathVariable String category) {
         BookCategory bookCategory = null;
@@ -41,9 +46,25 @@ public class BookController {
         return BOOK_SERVICE.getBooksByCategory(bookCategory);
     }
 
+    // 교재 제목으로 검색
     @GetMapping("/search")
     public List<BookDTO> getSearchBooksByTitle(@RequestParam String title) {
         return BOOK_SERVICE.getSearchBooksByTitle(title);
+    }
+
+
+    // 교재 개발
+    @PostMapping("/create")
+    public ResponseEntity<BookDTO> postCreateBook(@RequestBody BookDTO bookDTO) {
+        String title = bookDTO.getTitle();
+        String introduction = bookDTO.getIntroduction();
+
+        // 교재 개발
+        BookEntity createdBook = BOOK_SERVICE.postCreateBook(title, introduction);
+
+        // 생성된 교재를 DTO로 변환하여 반환
+        BookDTO createdBookDTO = BOOK_SERVICE.convertToDTO(createdBook);
+        return new ResponseEntity<>(createdBookDTO, HttpStatus.CREATED);
     }
 
 }
