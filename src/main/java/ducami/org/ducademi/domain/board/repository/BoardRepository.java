8b820084f5611ea49a58899663008883c5c5eddc
김호준 <hojoon4868@gmail.com>
2024-04-15 17:57:04 +0900

    Fix :: BoardRepository 수정

--- src/main/java/ducami/org/ducademi/domain/board/repository/BoardRepository.java
+++ src/main/java/ducami/org/ducademi/domain/board/repository/BoardRepository.java
@@ -3,8 +3,10 @@ package ducami.org.ducademi.domain.board.repository;
 import ducami.org.ducademi.domain.board.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {// 여기다가 회원인지 아닌지 확인하는 게 있어야 하나
-
+    List<BoardEntity> findAllByLectureIdx(Long lectureIdx);
 }
