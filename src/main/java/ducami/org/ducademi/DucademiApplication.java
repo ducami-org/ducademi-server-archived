cbe0e3c601476c38fcbce76da5a8ae53010d94a4
heunseoRyu <heunseo9@gmail.com>
2024-04-19 22:05:47 +0900

    Feat :: DucademiApplication 수정
    @EnableJpaAuditing이 2개 붙어있길래 하나 삭제

--- src/main/java/ducami/org/ducademi/DucademiApplication.java
+++ src/main/java/ducami/org/ducademi/DucademiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
