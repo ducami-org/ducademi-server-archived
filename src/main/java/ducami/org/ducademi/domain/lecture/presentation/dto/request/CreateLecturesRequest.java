f6eb6728435f62da832a56f4368b34639d7be84c
heunseoRyu <heunseo9@gmail.com>
2024-04-21 00:18:23 +0900

    Feat :: CreateLecturesRequest 수정
    일부 LocalDateTime -> LocalDate 로 수정

--- src/main/java/ducami/org/ducademi/domain/lecture/presentation/dto/request/CreateLecturesRequest.java
+++ src/main/java/ducami/org/ducademi/domain/lecture/presentation/dto/request/CreateLecturesRequest.java
@@ -2,10 +2,12 @@ package ducami.org.ducademi.domain.lecture.presentation.dto.request;
 
 
 import ducami.org.ducademi.domain.lecture.domain.Lecture;
+import ducami.org.ducademi.domain.lecture.domain.enums.LectureType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -17,13 +19,15 @@ public class CreateLecturesRequest {
     private String title;
     private String introduction;
     private String maxApplier;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private LocalDateTime applyStartDate;
     private LocalDateTime applyEndDate;
     private String target;
+    private LectureType category;
+    private Long teacherIdx;
 
-    public Lecture toEntity(){
+    public Lecture toEntity(Long idx){
         return Lecture.builder()
                 .title(this.title)
                 .introduction(this.introduction)
@@ -32,7 +36,8 @@ public class CreateLecturesRequest {
                 .endDate(this.endDate)
                 .applyStartDate(this.applyStartDate)
                 .applyEndDate(this.applyEndDate)
-                .target(this.target).build();
+                .target(this.target)
+                .teacherIdx(idx).build();
     }
 
 
