94e78239a0ae4b9b3f62a3a136a4ac1f66653ad6
heunseoRyu <heunseo9@gmail.com>
2024-04-21 00:22:19 +0900

    Feat :: LectureRepository 수정
    1. findAllByTeacherIdx() 정의
    2. findAllByCategory() 정의
    3. findAllByKeyword() 정의
    4. findByIdx() 정의

--- src/main/java/ducami/org/ducademi/domain/lecture/domain/repository/LectureRepository.java
+++ src/main/java/ducami/org/ducademi/domain/lecture/domain/repository/LectureRepository.java
@@ -1,7 +1,22 @@
 package ducami.org.ducademi.domain.lecture.domain.repository;
 
 import ducami.org.ducademi.domain.lecture.domain.Lecture;
+import ducami.org.ducademi.domain.lecture.domain.enums.LectureType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LectureRepository extends JpaRepository<Lecture,Long> {
+
+    @Query("SELECT l FROM Lecture l WHERE l.teacherIdx = :teacherIdx")
+    Page<Lecture> findAllByTeacherIdx(Pageable pageable,Long teacherIdx);
+
+    @Query("SELECT l FROM Lecture l WHERE l.category = :category")
+    Page<Lecture> findAllByCategory(Pageable pageable, LectureType category);
+
+    @Query("SELECT l FROM Lecture l WHERE l.title LIKE %:keyword%")
+    Page<Lecture> findAllByKeyword(Pageable pageable, String keyword);
+
+    Lecture findByIdx(long idx);
 }
